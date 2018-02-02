ffbfed54a89ed34324a6036de2faff9e82e5f45b
Prasanth Kurian <prasanthkurian@gmail.com>
2018-02-02 14:16:00 +0000

    some more

---
+++ static/webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const config = {
+    entry:  __dirname + '/js/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+};
+module.exports = config;
+
